31129df9171bc340a0bea231ead895e91eb3f6bc
Jack5201314 <53046279+Jack5201314@users.noreply.github.com>
2020-10-13 12:49:02 +0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,11 @@
 public class HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello Git");
+        print();
     }
+    public static void print(){
+        System.out.println("去你妈的");
+    }
+                       
 
 }
