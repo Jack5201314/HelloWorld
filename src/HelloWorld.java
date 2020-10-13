b588d67e620d98fdf1bbcc76b41564bd8477117f
zhangxiaofeng <2385170477>
2020-10-13 12:41:51 +0800

    修改之后

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,9 +8,6 @@ public class HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello Git");
         System.out.println("去你妈的！");
-        print();
     }
-    public static void print(){
-        System.out.println("草泥马");
-    }
+
 }
