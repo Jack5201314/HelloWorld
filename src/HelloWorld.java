bcbafa40f27b34439efbed502a206ec3d6283319
zhangxiaofeng <2385170477>
2020-10-13 12:30:00 +0800

    第一次使用Git

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @program: HelloWorld
+ * @description: GitTest
+ * @author: 张小锋
+ * @create: 2020-10-13 12:14
+ **/
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello Git");
+    }
+}
